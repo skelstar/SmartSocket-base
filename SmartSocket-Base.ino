df97ba6c309a82744f4c75e2a677d637311c9958
skelstar <skelstar@gmail.com>
2017-05-20 05:40:17 +1200

    mqtt button press, mqtt led turn on/off

--- SmartSocket-Base.ino
+++ SmartSocket-Base.ino
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <myWifiHelper.h>
 #include <myPushButton.h>
+#include <ArduinoJson.h>            // https://github.com/bblanchon/ArduinoJson
 
 char versionText[] = "SmartSocket-base v0.9";
 
@@ -11,6 +12,13 @@ char versionText[] = "SmartSocket-base v0.9";
 #define BLUE_LED        4
 #define LED_ON          LOW
 #define LED_OFF         HIGH
+
+#define     TOPIC_EVENT     "/smartsocket-base/event"
+#define     TOPIC_COMMAND   "/smartsocket-base/command"
+
+
+#define     EVENT_BUTTON_PUSHED     "button pushed"
+#define     EVENT_BUTTON_HELD       "button held"
 
 /* ----------------------------------------------------------- */
 #define     WIFI_HOSTNAME "SmartSocket-base"
@@ -28,9 +36,11 @@ void button_callback(int eventCode, int eventParam) {
             break;
         case button.EV_HELD_FOR_LONG_ENOUGH:
             Serial.println("EV_HELD_FOR_LONG_ENOUGH");
+            wifiHelper.mqttPublish(TOPIC_EVENT, EVENT_BUTTON_HELD);
             break;
         case button.EV_RELEASED:
             Serial.println("EV_RELEASED");
+            wifiHelper.mqttPublish(TOPIC_EVENT, EVENT_BUTTON_PUSHED);
             break;
         case button.EV_RELEASED_FROM_HELD_TIME:
             Serial.println("EV_RELEASED_FROM_HELD_TIME");
@@ -40,6 +50,25 @@ void button_callback(int eventCode, int eventParam) {
     }
 }
 //--------------------------------------------------------------------------------
+void mqttcallback_Command(byte *payload, unsigned int length) {
+
+    const char* command = wifiHelper.mqttGetJsonCommand(payload);
+    const char* value = wifiHelper.mqttGetJsonCommandValue();
+
+    // Serial.print("command: "); Serial.println(command);
+    // Serial.print("value: "); Serial.println(value);
+
+    if (strcmp(command, "LED") == 0) {
+        if (strcmp(value, "ON") == 0) {
+            digitalWrite(BLUE_LED, LOW);
+        }
+        else if (strcmp(value, "OFF") == 0) {
+            digitalWrite(BLUE_LED, HIGH);
+        }
+    }
+}
+
+//--------------------------------------------------------------------------------
 
 void setup() {
 
@@ -48,10 +77,15 @@ void setup() {
     Serial.println("Booting");
     Serial.println(versionText);
 
+    pinMode(BLUE_LED, OUTPUT);
+    digitalWrite(BLUE_LED, HIGH);
+
 
     wifiHelper.setupWifi();
     wifiHelper.setupOTA(WIFI_HOSTNAME);
     wifiHelper.setupMqtt();
+
+    wifiHelper.mqttAddSubscription(TOPIC_COMMAND, mqttcallback_Command);
 }
 
 /* ----------------------------------------------------------- */
